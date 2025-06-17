99cd8184807586e9311f2e50bf0aaef088a6504c
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-17 09:33:52 +0200

    Feature: add BaseTest for E2E tests

---
+++ src/test/java/E2E/BaseTest.java
@@ -0,0 +1,23 @@
+package E2E;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:5173/");
+    }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+}
