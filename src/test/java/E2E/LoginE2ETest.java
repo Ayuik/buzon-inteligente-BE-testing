ef1ce24b81d7f5f058b251963817a72159f9e902
AlejandroAriasSL <alejandroasilva02@gmail>
2025-06-17 09:57:00 +0200

    Test: Should load HomePage

---
+++ src/test/java/E2E/LoginE2ETest.java
@@ -0,0 +1,29 @@
+package E2E;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginE2ETest extends BaseTest {
+
+    @Test
+    @DisplayName("Should load HomePage")
+    void test_should_load_homepage(){
+
+        List<WebElement> buttonElements = driver.findElements(By.tagName("button"));
+
+        assertThat(buttonElements, hasSize(2));
+        assertThat(buttonElements.stream().anyMatch(x -> x.getText().equals("Iniciar sesión")), is(true));
+        assertThat(buttonElements.stream().anyMatch(x -> x.getText().equals("Registrarse")), is(true));
+    }
+
+
+}
